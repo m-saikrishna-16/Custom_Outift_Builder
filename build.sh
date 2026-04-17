57b98e65e44a8940f7c4739f8d20574228b5e0d0
m-saikrishna-16 <162353885+m-saikrishna-16@users.noreply.github.com>
2026-04-17 14:53:02 +0530

    Add makemigrations command to build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
 
 # ✅ CREATE SUPERUSER (only if not exists)
