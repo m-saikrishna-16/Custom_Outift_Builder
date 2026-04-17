624e9495b28a85339fed2b48d16d7aedaad8229d
m-saikrishna-16 <162353885+m-saikrishna-16@users.noreply.github.com>
2026-04-17 12:02:34 +0530

    Update settings.py

--- fashion/settings.py
+++ fashion/settings.py
@@ -142,8 +142,8 @@ APPEND_SLASH = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 import os
 
