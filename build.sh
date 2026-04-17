2b5d5684f125b01c642d35a329e8ac7959057556
m-saikrishna-16 <162353885+m-saikrishna-16@users.noreply.github.com>
2026-04-17 12:43:00 +0530

    Enhance build script to create superuser if needed
    Added superuser creation script to build process.

--- build.sh
+++ build.sh
@@ -2,5 +2,17 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# ✅ CREATE SUPERUSER (only if not exists)
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+import os;
+username = os.getenv('DJANGO_SUPERUSER_USERNAME');
+email = os.getenv('DJANGO_SUPERUSER_EMAIL');
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD');
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+" | python manage.py shell
