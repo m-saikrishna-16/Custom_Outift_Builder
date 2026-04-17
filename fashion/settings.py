5253836eeb579cc8137dad14d0eced884795770b
m-saikrishna-16 <162353885+m-saikrishna-16@users.noreply.github.com>
2026-04-17 11:07:43 +0530

    Update settings.py

--- fashion/settings.py
+++ fashion/settings.py
@@ -148,6 +148,8 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
